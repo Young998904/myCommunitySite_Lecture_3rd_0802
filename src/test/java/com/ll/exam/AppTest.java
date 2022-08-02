7055ee3571210692f283432ce002e81f7ec83e02
Young998904 <like0998904@gmail.com>
2022-08-02 10:27:19 +0900

    articleController를 Container를 통해서 얻는다 & articleController와 같이 1개만 필요한 것은 싱글톤으로 작동

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,4 +1,30 @@
 package com.ll.exam;
 
+import com.ll.exam.article.controller.ArticleController;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AppTest {
+    @Test
+    public void junit_assertThat() {
+        int rs = 10 + 20;
+
+        assertThat(rs).isEqualTo(30);
+    }
+
+    @Test
+    public void ioc__articleController() {
+        ArticleController articleController = Container.getArticleController();
+
+        assertThat(articleController).isNotNull();
+    }
+
+    @Test
+    public void ioc__articleController__Singleton() {
+        ArticleController articleController1 = Container.getArticleController();
+        ArticleController articleController2 = Container.getArticleController();
+
+        assertThat(articleController2).isEqualTo(articleController1);
+    }
 }
